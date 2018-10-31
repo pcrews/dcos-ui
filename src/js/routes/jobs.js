50ffc16f4d7c8fa2218d9086e49591b0f3db2049
Daniel Schmidt <dschmidt@mesosphere.com>
2018-10-30 17:31:11 -0700

    POC: graphiQL

--- src/js/routes/jobs.js
+++ src/js/routes/jobs.js
@@ -12,6 +12,51 @@ import TaskLogsContainer from "#PLUGINS/services/src/js/pages/task-details/TaskL
 import JobDetailPageContainer from "#PLUGINS/jobs/src/js/JobDetailPageContainer";
 import JobsTaskDetailPage from "#PLUGINS/jobs/src/js/pages/JobTaskDetailPage";
 import JobsPage from "../pages/JobsPage";
+import JobModel from "#PLUGINS/jobs/src/js/data/JobModel";
+import { graphqlObservable } from "data-service";
+import gql from "graphql-tag";
+
+import GraphiQL from "graphiql";
+import "graphiql/graphiql.css";
+
+function GraphiQLWrapper() {
+  // our graphql has no introspection currently, so we need to get the schema
+  // we need to have some sort of "select" for this somewhere (we also got the repositories model)
+
+  const schema = JobModel;
+  // fetcher seems to be able to handle observables
+  const fetcher = ({ query, variables }) => {
+    const q = gql`
+      ${query}
+    `;
+
+    return graphqlObservable(q, schema, variables);
+  };
+
+  return (
+    <div id="isolator" style={{ width: "100%" }}>
+      <GraphiQL
+        defaultQuery={`{
+            jobs(path: "") {
+              path
+              filteredCount
+              totalCount
+              nodes
+            }
+          }
+      `}
+        fetcher={fetcher}
+        schema={schema}
+      />
+    </div>
+  );
+}
+
+GraphiQLWrapper.routeConfig = {
+  label: "GraphiQL",
+  icon: <span>🚒</span>,
+  matches: /^\/development/
+};
 
 const jobsRoutes = [
   {
@@ -21,7 +66,7 @@ const jobsRoutes = [
   },
   {
     type: Route,
-    component: JobsPage,
+    component: GraphiQLWrapper,
     path: "jobs",
     category: "root",
     isInSidebar: true,
